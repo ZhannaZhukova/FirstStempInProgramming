f7b772a3f7f69124cea50e467fd639e200678889
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-11 22:39:18 +0700

    Add new solution for Task4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,11 @@
+﻿string? inputLine = Console.ReadLine ();
+
+if (inputLine != null)
+{
+    //int inputNumber = int.Parse (inputLine);
+    //int outNumber = inputNumber % 10;
+    //int lastNum=int.Parse(inputLine.Substring(2,1));
+    //Console.WriteLine (lastNum);
+    char[]M=inputLine.ToCharArray();
+    Console.WriteLine(M[2]);
+}
