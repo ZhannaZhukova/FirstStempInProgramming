759ec32e62b97849db4a1f755520218207b254ed
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-11 20:14:10 +0700

    Add Solution for Task2

---
+++ SolutionTask2/Program.cs
@@ -0,0 +1,17 @@
+﻿string? inputLine = Console.ReadLine ();
+
+if (inputLine != null)
+{
+    int inputNumber = int.Parse (inputLine);
+    string[]dayOfWeek=new string[7];
+    dayOfWeek[0]="понедельник";
+    dayOfWeek[1]="вторник";
+    dayOfWeek[2]="среда";
+    dayOfWeek[3]="четверг";
+    dayOfWeek[4]="пятница";
+    dayOfWeek[5]="суббота";
+    dayOfWeek[6]="воскресенье";
+    
+
+    Console.WriteLine (dayOfWeek[inputNumber-1]);
+}
