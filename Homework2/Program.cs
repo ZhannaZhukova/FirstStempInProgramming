22e6bd78db27103fc4f902f40d9817bf668edb35
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-12 11:10:07 +0700

    Add solution for task in homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,16 @@
+﻿string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse (inputLine);
+       
+    if(inputNumber%2==0)
+    {
+        Console.WriteLine ("четное");
+    }
+    else
+    {
+        Console.WriteLine ("нечетное");
+    }
+}
+
+   
