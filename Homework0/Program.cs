b5f65b70bb43cdf7d06b2d4f27fe7f00dd383ac6
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-12 08:01:53 +0700

    Add solution for task in homework0

---
+++ Homework0/Program.cs
@@ -0,0 +1,16 @@
+﻿string? inputLineOne = Console.ReadLine();
+string? inputLineTwo = Console.ReadLine();
+if(inputLineOne !=null && inputLineTwo !=null)
+{
+    int inputNumberOne = int.Parse(inputLineOne);
+    int inputNumberTwo = int.Parse(inputLineTwo);
+
+    if(inputNumberOne > inputNumberTwo)
+    {
+        Console.WriteLine (inputNumberOne);        
+    }
+    else{
+        Console.WriteLine (inputNumberTwo);
+    }
+}
+
