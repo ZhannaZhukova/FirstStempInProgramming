f0f2060339592e96d0381f03a1ae59966edb6372
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-17 00:44:34 +0700

    Add solution for task in homework5

---
+++ Homework5/Program.cs
@@ -0,0 +1,12 @@
+﻿string?inputLineOne=Console.ReadLine();
+int inputNumberOne=int.Parse(inputLineOne);
+char[]digit=inputLineOne.ToCharArray();
+if(inputNumberOne>99)
+{
+    
+    Console.WriteLine(digit[2]);
+}
+else
+{
+    Console.WriteLine("Такой цифры нет");
+}
