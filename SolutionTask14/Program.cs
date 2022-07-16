fe6880d6fdc4d2fe43df4dfd17f79a71be2e9f36
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-16 23:08:10 +0700

    Add solution fot Task14

---
+++ SolutionTask14/Program.cs
@@ -0,0 +1,14 @@
+﻿string?inputLineOne=Console.ReadLine();
+
+if (inputLineOne!=null)
+{
+    int num=int.Parse(inputLineOne);
+    
+    if(num%7==0&&num%23==0)
+    {
+        Console.WriteLine("кратно");
+    }
+    else{
+        Console.WriteLine("не кратно");
+    }
+}
