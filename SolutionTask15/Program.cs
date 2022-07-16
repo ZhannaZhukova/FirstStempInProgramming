96b4267c9a7034d86476927e8082078e74ecfabe
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-16 23:29:22 +0700

    Add solution fot Task15

---
+++ SolutionTask15/Program.cs
@@ -0,0 +1,15 @@
+﻿string?inputLineOne=Console.ReadLine();
+string?inputLineTwo=Console.ReadLine();
+if(inputLineOne!=null&&inputLineTwo!=null)
+{
+    int inputNumberOne=int.Parse(inputLineOne);
+    int inputNumberTwo=int.Parse(inputLineTwo);
+    if(inputNumberOne%inputNumberTwo==0||inputNumberTwo%inputNumberOne==0)
+    {
+        Console.WriteLine("Yes");
+    }
+    else
+    {
+        Console.WriteLine("No");
+    }
+}
