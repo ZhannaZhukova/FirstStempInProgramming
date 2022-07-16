34ccdd021bd670691d1391b0298f7753b4bf02e1
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-16 22:00:56 +0700

    Add solution fot Task11

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,20 @@
+﻿//1 Вариант
+//int number=new Random().Next(100,1000);
+//Console.WriteLine("Число: "+number);
+//int numberSecondDigit=number/100;
+//int numberThirdDigit=number%10;
+//Console.WriteLine("Число после удаления второй цифры: "+numberSecondDigit+numberThirdDigit);
+
+//2 Вариант
+void middleDiggitCutter()
+{
+    int number=new Random().Next(100,1000);
+    Console.Write("The random number is ");
+    Console.WriteLine(number);
+    string stringNum=number.ToString();
+    Console.Write("The modified number is ");
+    Console.Write(stringNum[0]);
+    Console.Write(stringNum[1]);
+}
+
+middleDiggitCutter();
