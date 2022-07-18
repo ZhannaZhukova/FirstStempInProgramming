ce8eee11c411be914c1e6e0ddb6469295fc03d16
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-19 00:08:21 +0700

    Для Ильи2

--- SolutionTask17/Program.cs
+++ SolutionTask17/Program.cs
@@ -1,17 +1,17 @@
 ﻿//Метод считывает строчки и возвращает массив с ними
 int [,]readPoint()
 {
-    string inputLine=Console.ReadLine();
+    string inputLine = Console.ReadLine();
     //x=34;y=-30 ->4
-    string coordXLine=inputLine.Substring(0,inputLine.IndexOf(";"));
-    coordXLine=coordXLine.Substring(coordXLine.IndexOf("=")+1);
-    string coordYLine=inputLine.Substring(inputLine.IndexOf(";")+1);
-    coordYLine=coordYLine.Substring(coordXLine.IndexOf("=")+1);
-    int coordX=int.Parse(coordXLine);
-    int coordY=int.Parse(coordYLine);
-    int[,]arrayOut=new int[1,2];
-    arrayOut[0,0]=coordX;
-    arrayOut[0,1]=coordY;
+    string coordXLine = inputLine.Substring(0, inputLine.IndexOf(";"));
+    coordXLine = coordXLine.Substring(coordXLine.IndexOf("=") + 1);
+    string coordYLine = inputLine.Substring(inputLine.IndexOf(";") + 1);
+    coordYLine = coordYLine.Substring(coordXLine.IndexOf("=") + 1);
+    int coordX = int.Parse(coordXLine);
+    int coordY = int.Parse(coordYLine);
+    int[,] arrayOut = new int[1, 2];
+    arrayOut[0, 0] = coordX;
+    arrayOut[0, 1] = coordY;
     return arrayOut;
 }
 //Печатает номер четверти
@@ -26,5 +26,5 @@ void printQuater(int[,]arreyPoint)
     if (arreyPoint[0,0]>0&&arreyPoint[0,1]<0)
     Console.WriteLine("4 четверть");
 }
-readPoint();
+int[,]arreyPoint=readPoint();
 printQuater(arreyPoint);
