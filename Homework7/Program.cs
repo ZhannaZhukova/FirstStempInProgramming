66641ad387659c855424ce8b4a298a10844ed91e
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-19 21:04:58 +0700

    Add solution for task 22 Расстояние между 2 точками в 3D пространстве

---
+++ Homework7/Program.cs
@@ -0,0 +1,41 @@
+﻿//Программа, которая принимает на вход координаты 2 точек и находит расстояние между ними в 3D пространстве.
+int coordXPointA;
+int coordYPointA;
+int coordZPointA;
+int coordXPointB;
+int coordYPointB;
+int coordZPointB;
+double lenghtAB;
+
+// Метод ввода данных от пользователя (считывает координаты точек А,В)
+void readDataOfPoin ()
+{
+    Console.WriteLine("Введите координату Х точки А");
+    coordXPointA=int.Parse (Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки А");
+    coordYPointA=int.Parse (Console.ReadLine());
+
+    Console.WriteLine("Введите координату Z точки А");
+    coordZPointA=int.Parse (Console.ReadLine());
+
+    Console.WriteLine("Введите координату Х точки B");
+    coordXPointB=int.Parse (Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки B");
+    coordYPointB=int.Parse (Console.ReadLine());
+
+    Console.WriteLine("Введите координату Z точки B");
+    coordZPointB=int.Parse (Console.ReadLine());
+}
+
+//Вычисляет расстояние между А,В
+void conculateLenghtAB()
+{
+    lenghtAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB),2) + Math.Pow ((coordYPointA-coordYPointB),2)+ Math.Pow ((coordZPointA-coordZPointB),2));
+
+}
+
+readDataOfPoin ();
+conculateLenghtAB();
+ Console.WriteLine (lenghtAB);
