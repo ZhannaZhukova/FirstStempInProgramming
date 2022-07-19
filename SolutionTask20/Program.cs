96dcf55f99fc9fd0371b7685567c9b878dce6164
Zhanna <zhuzha.jukova@yandex.ru>
2022-07-19 18:44:33 +0700

    Add solution for task 20

---
+++ SolutionTask20/Program.cs
@@ -0,0 +1,31 @@
+﻿// Метод ввода данных от пользователя (считывает координаты точек А,В)
+int coordXPointA;
+int coordYPointA;
+int coordXPointB;
+int coordYPointB;
+double lenghtAB;
+void readDataOfPoin ()
+{
+    Console.WriteLine("Введите координату Х точки А");
+    coordXPointA=int.Parse (Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки А");
+    coordYPointA=int.Parse (Console.ReadLine());
+
+    Console.WriteLine("Введите координату Х точки B");
+    coordXPointB=int.Parse (Console.ReadLine());
+
+    Console.WriteLine("Введите координату Y точки B");
+    coordYPointB=int.Parse (Console.ReadLine());
+}
+
+//Вычисляет расстояние между А,В
+void conculateLenghtAB()
+{
+    lenghtAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB),2) + Math.Pow ((coordYPointA-coordYPointB),2));
+
+}
+
+readDataOfPoin ();
+conculateLenghtAB();
+ Console.WriteLine (lenghtAB);
