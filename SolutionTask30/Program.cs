a17011765eaf85080411ffe8d61b8ff9632a2e4b
Zhanna <zhuzha.jukova@yandex.ru>
2022-08-04 22:15:10 +0700

    Add solution for task 30

---
+++ SolutionTask30/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая выводит массив из 8 элементов, заполненный 0 и 1 в случайном порядке.
+System.Random numberSintezator = new Random();
+
+void VariantNaive()
+{
+    int i=0;
+    Console.Write ("[");
+    while (i<8-1)
+    {
+        Console.Write (numberSintezator.Next(0,2)+",");
+        i++;
+    }
+    Console.Write (numberSintezator.Next(0,2));
+    Console.Write ("]");
+   
+}
+VariantNaive();
