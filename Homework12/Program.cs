a6c9292ff81c4beaaf2cc98e94149e734f12c195
Zhanna <zhuzha.jukova@yandex.ru>
2022-08-05 10:09:04 +0700

    Add solution for Homework12 Массив из 8 элементов

---
+++ Homework12/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая создает массив из 8 элементов и выводит их на экран
+System.Random numberSintezator = new Random();
+
+void VariantNaive()
+{
+    int i=0;
+    Console.Write ("[");
+    while (i<8-1)
+    {
+        Console.Write (numberSintezator.Next(0,9)+",");
+        i++;
+    }
+    Console.Write (numberSintezator.Next(0,9));
+    Console.Write ("]");
+   
+}
+VariantNaive();
