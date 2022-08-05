c7d1395c15bef8490987d2f62081a0f7fdd891b7
Zhanna <zhuzha.jukova@yandex.ru>
2022-08-05 10:08:20 +0700

    Add solution for Homework11 Сумма чисел в числе

---
+++ Homework11/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму чисел в числе
+//Запрашиваем данные пользователя и парсим их
+string?a= Console.ReadLine();
+int b = 0;
+int numberA= int.Parse(a);
+//Производим вычисления суммы чисел
+void Conculate()
+{
+    while (numberA>0)
+{
+    b=b+numberA%10;
+    numberA=numberA/10;
+}
+}
+//Выводим результат
+Conculate();
+Console.WriteLine(b);
