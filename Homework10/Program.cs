47e0854464d80031f82639f1fc392e05d36c9b73
Zhanna <zhuzha.jukova@yandex.ru>
2022-08-05 23:07:50 +0700

    Add solution for Homework10

---
+++ Homework10/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход 2 числа А и В и возводит число А в натуральную степень В.
+//Запрашиваем данные от пользователя и парсим их
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+
+//Вычисляем степень числа и выводим ответ:
+
+void Conculate()
+{
+    int result=1;
+    for (int i=0; i<b; i++ )
+    {
+        result*=a;
+    }
+    Console.WriteLine(result);
+}
+
+Conculate ();
+
+
+
+
+
+
+
