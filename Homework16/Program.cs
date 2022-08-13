8f9e30c292256b875d6a424676a6ca73fe0df378
Zhanna <zhuzha.jukova@yandex.ru>
2022-08-13 15:59:19 +0700

    Solution for Homework 16

---
+++ Homework16/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write ("Введите элементы через пробел: ");
+int [] inputArray = Array.ConvertAll (Console.ReadLine().Split(), int.Parse);
+int count =0;
+void Conculate()
+{
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine ("Количество элементов >0: " +count);
+}
+Conculate();
