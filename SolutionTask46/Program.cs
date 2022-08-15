b71beea11c72012508d9478088112841691015e8
Zhanna <zhuzha.jukova@yandex.ru>
2022-08-15 23:29:58 +0700

    Add solution for Task 46 Двумерный цветной массив

---
+++ SolutionTask46/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задайте двумерный массив m*n, заполненный случайными целыми числами
+
+//Метод для заполнения двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    //создаем экземпляр типа рандомайзер
+    System.Random numberSintezator = new System.Random();
+
+    //буфферные переменные строки и столбца
+    int i = 0; int j = 0;
+
+    //создаем массив
+    int[,] outArray = new int[countRow, countColumn];
+    
+    //цикл заполнения массива сначала по столбцам, потом по строчкам
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSintezator.Next(0, 101); 
+            j++;
+        }
+        
+        i++;
+    }
+    //возвращаем значение
+    return outArray;
+}
+//Метод печатает двумерныймассив
+void PrintTwoDimArray(int[,] inputArray)
+{
+    //Буфферная переменная
+    int i = 0; int j = 0;
+       
+    //цикл заполнения массива сначала по столбцам, потом по строчкам
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.Write(inputArray[i,j] + " "); 
+            j++;
+        }
+        //Выводим конец строки
+        //Console.Write("\n");
+        //или
+        Console.WriteLine();
+        i++;
+    }
+}
+ConsoleColor[] col = new ConsoleColor[] {ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,ConsoleColor.Yellow};
+//Модифицированный метод печати
+void PrintColorTwoDimArray(int[,] inputArray)
+{
+   //Буфферная переменная
+    int i = 0; int j = 0;
+    //Раскрашиваем в желтый
+    //Console.ForegroundColor = ConsoleColor.Yellow;   
+    //цикл заполнения массива сначала по столбцам, потом по строчкам
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.ForegroundColor = col [new System.Random().Next(0,16)];
+            Console.Write(inputArray[i,j] + " "); 
+            Console.ResetColor();
+            j++;
+        }
+        //Выводим конец строки
+        //Console.Write("\n");
+        //или
+        Console.WriteLine();
+        i++;
+    }
+    //Скидываем настройку цвета консоли
+    //Console.ResetColor();
+}
+
+int[,] twoDimArray = FillTwoDimArray(3, 4);
+PrintTwoDimArray(twoDimArray);
+PrintColorTwoDimArray(twoDimArray);
