20ecd8b9d1fd88d88e1abeed74c41bd3928c26be
Zhanna <zhuzha.jukova@yandex.ru>
2022-08-19 22:44:18 +0700

    Add solution for homework21 Меняем элементы строки по убыванию

---
+++ Homework21/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+//которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Метод для заполнения двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    //создаем экземпляр типа рандомайзер
+    System.Random numberSintezator = new System.Random();
+
+    //буфферные переменные строки и столбца
+    int i = 0; int j = 0;
+
+    //создаем массив
+    int[,] outArray = new int[countRow, countColumn];
+
+    //цикл заполнения массива сначала по столбцам, потом по строчкам
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSintezator.Next(0, 10);
+            j++;
+        }
+
+        i++;
+    }
+    //возвращаем значение
+    return outArray;
+}
+//Метод печатает двумерныймассив
+void PrintTwoDimArray(int[,] inputArray)
+{
+    //Буфферная переменная
+    int i = 0; int j = 0;
+
+    //цикл заполнения массива сначала по столбцам, потом по строчкам
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.Write(inputArray[i, j] + " ");
+            j++;
+        }
+        //Выводим конец строки
+        //Console.Write("\n");
+        //или
+        Console.WriteLine();
+        i++;
+    }
+}
+
+//Метод сортировки строки по убыванию
+int[,] FilterArray(int[,] inputArray)
+{
+    int bufferElement = 0;
+    int i = 0; int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            for (int filter = j + 1; filter < inputArray.GetLength(1); filter++)
+            {
+                if (inputArray[i, j] < inputArray[i, filter])
+                {
+                    bufferElement = inputArray[i, filter];
+                    inputArray[i, filter] = inputArray[i, j];
+                    inputArray[i, j] = bufferElement;
+                }
+            }
+            j++;
+        }
+        i++;
+    }
+    return inputArray;
+}
+
+
+int[,] twoDimArray = FillTwoDimArray(3, 4);
+PrintTwoDimArray(twoDimArray);
+Console.WriteLine();
+
+PrintTwoDimArray(FilterArray(twoDimArray));
+
+
