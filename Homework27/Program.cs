c248aa9c02feb6a28f306fd2276a86ad85609e0b
Zhanna <zhuzha.jukova@yandex.ru>
2022-08-21 22:44:03 +0700

    Add solution for homework27 Сумма чисел от M до N рекурсия

---
+++ Homework27/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+int sum = 0;
+int numberM = ReadData("Задайте число M");
+int numberN = ReadData("Задайте число N");
+
+if (numberM < numberN)
+{
+    SumNaturalNamberPrinter(numberM, numberN);
+}
+else
+{
+    SumNaturalNamberPrinter(numberN, numberM);
+}
+
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+
+
+
+int SumNaturalNamberPrinter(int m, int n)
+{
+    if (m - 1 == n) return m;
+    sum = sum + m;
+    SumNaturalNamberPrinter(m + 1, n);
+    return sum;
+}
+Console.WriteLine(sum);
